7faf2134aaa70026c0232d6b06ded352d88f3497
JasonQu <qjpcpu@gmail.com>
2018-07-27 10:36:28 +0800

    update

--- cmd/printer/main.go
+++ cmd/printer/main.go
@@ -3,6 +3,7 @@ package main
 // use siddontang go-mysql commit 535abe8f2eba8d1edaf1fecacea288ebb3b479ad
 
 import (
+	"fmt"
 	"github.com/qjpcpu/binlog-canal/config"
 	"github.com/qjpcpu/binlog-canal/sync"
 	"log"
@@ -12,16 +13,36 @@ import (
 )
 
 func main() {
+	user := os.Getenv("DB_USER")
+	pwd := os.Getenv("DB_PWD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	db_name := os.Getenv("DB_NAME")
+	table := os.Getenv("DB_TABLE")
+	if user == "" {
+		user = "root"
+	}
+	if host == "" {
+		host = "127.0.0.1"
+	}
+	if port == "" {
+		port = "3306"
+	}
+	if db_name == "" || table == "" {
+		fmt.Println("no DB_NAME or DB_TALBE found in env")
+		os.Exit(1)
+	}
+	fmt.Printf("will print %s.%s data change\n\n", db_name, table)
 	cfg := config.ServerConfig{
 		SourceConfig: config.SourceConfig{
-			MysqlConn: "USER:PASSWORD@tcp(127.0.0.1:3308)/?charset=utf8mb4&parseTime=true",
+			MysqlConn: fmt.Sprintf("%s:%s@tcp(%s:%s)/?charset=utf8mb4&parseTime=true", user, pwd, host, port),
 			SyncAll:   false,
 			Sources: []config.Source{
 				{
-					Schema: "test_db",
+					Schema: db_name,
 					Tables: []config.TopicInfo{
 						{
-							Table: "test_table",
+							Table: table,
 						},
 					},
 				},
